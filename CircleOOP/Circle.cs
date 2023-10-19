19064374eaef9cb5d347b4b5005174babef1631e
Andreas A <adde1990@live.se>
2023-10-19 10:46:38 +0200

    Add project files.

---
+++ CircleOOP/Circle.cs
@@ -0,0 +1,19 @@
+﻿namespace CircleOOP
+{
+    public class Circle
+    {
+
+        private double _Radius;
+
+        public Circle(double radie)
+        {
+            _Radius = radie;
+        }
+
+        public float getArea() => (float)(_Radius * _Radius * Math.PI);
+
+    }
+
+}
+
+
---
+++ CircleOOP/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CircleOOP
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Circle circleRad5 = new Circle(5);
+            Console.WriteLine($"Area: {Math.Round(circleRad5.getArea(), 1)}");
+
+            Circle circleRad6 = new Circle(6);
+            Console.WriteLine($"Area: {Math.Round(circleRad6.getArea(), 1)}");
+
+        }
+    }
+}
